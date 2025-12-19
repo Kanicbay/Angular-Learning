e8782a3f40682dd9252091a51f1dc38bb5a5fac7
Brian Coyago <briancoyago@hotmail.com>
2025-12-18 19:16:20 -0500

    feat(angular21): customize the details page
    Brian Coyago

--- Angular21/hello_world/src/app/housing-location/housing-location.ts
+++ Angular21/hello_world/src/app/housing-location/housing-location.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
 import { HousingLocationInfo } from '../housing-location';
+import {RouterLink} from '@angular/router';
+
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterLink],
   template: `
     <section class="listing">
       <img
@@ -14,6 +16,7 @@ import { HousingLocationInfo } from '../housing-location';
       />
       <h2 class="listing-heading">{{ housingLocation().name }}</h2>
       <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+      <a [routerLink]="['/details', housingLocation().id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.css',

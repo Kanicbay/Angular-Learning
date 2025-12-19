fc25ec46370a1a6c121cfae28cf0767f2cdf5167
Brian Coyago <briancoyago@hotmail.com>
2025-12-18 19:23:40 -0500

    feat(angular21): add search functionality
    Brian Coyago

--- Angular21/hello_world/src/app/home/home.ts
+++ Angular21/hello_world/src/app/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
-import {HousingService} from '../housing.service';
+import { HousingService } from '../housing.service';
 import { HousingLocationInfo } from '../housing-location';
 
 @Component({
@@ -9,12 +9,12 @@ import { HousingLocationInfo } from '../housing-location';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      @for (housingLocation of housingLocationList; track $index) {
+        @for (housingLocation of filteredLocationList; track $index) {
         <app-housing-location [housingLocation]="housingLocation" />
       }
     </section>
@@ -25,9 +25,21 @@ export class Home {
 
   housingLocationList: HousingLocationInfo[] = [];
   housingService: HousingService = inject(HousingService);
-  
+  filteredLocationList: HousingLocationInfo[] = [];
+
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 
 }
